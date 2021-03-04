2d6184da27cc8dcefa01493a635b8fc8e9612876
Kento <46371150+kent7k@users.noreply.github.com>
2021-03-04 14:11:09 +0900

    Update micropost.rb

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
-class Micropost < ActiveRecord::Base
+class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, length: { maximum: 140 }, # 140文字の文字数制限
-                      presence: true # マイクロポストのコンテンツが存在しているかどうか
+                      presence: true # マイクロポストのコンテンツの存在確認
 end
