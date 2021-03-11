a9d56f4a8297752a1637d0380cfa28fb422b6772
kent7k <kent.ki720@gmail.com>
2021-03-12 08:29:40 +0900

    test: microposts_destroy_spec

---
+++ spec/requests/microposts_destroy_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+describe MicropostsController, type: :system do
+ let(:user) { FactoryBot.build(:user) }
+ before do
+  @micropost1 = FactoryBot.create(:micropost, user: user)
+ end
+ describe 'GET #destroy' do
+  subject(:action) do
+   get microposts_path
+   response
+  end
+  it { is_expected.to have_http_status(:ok) }
+  it 'マイクロポストに「削除」ボタンがあることを確認する' do
+   expect(action.body).to have_link 'Destroy'
+  end
+ end
+end
