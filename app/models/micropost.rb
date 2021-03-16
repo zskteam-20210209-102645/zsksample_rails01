5bbd8b0db89be70f20374e76600a0327cda83f66
masakichi-eng <makisankonan@gmail.com>
2021-03-16 12:11:39 +0900

    fix/micropost/model2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,5 @@ class Micropost < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   validates :content, length: { maximum: 140 },
-                      presence: true, unless: :was_attached?
+                      presence: true
 end
